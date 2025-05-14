dc942453440c10b20d80b40658daac400e661ca5
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2025-05-14 09:50:38 -0400

    Adding everything back to repo

--- ilab_triton/model_repository/identity_demo_model/identity_model_torchscript.py
+++ ilab_triton/model_repository/identity_demo_model/identity_model_torchscript.py
@@ -11,5 +11,5 @@ example_input = torch.randn(1, 3, 224, 224)  # Example input
 traced_model = torch.jit.trace(model, example_input)
 traced_model.save(
     '/raid/ilab/ilab-triton/ilab_triton/model_repository/' + \
-    'identity_demo_model/1/identity_demo_model.pt'
+    'identity_demo_model/1/model.pt'
 )
