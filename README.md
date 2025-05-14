283cdd5266fb4ceae22ffdcca303c9f0655c512a
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2025-05-14 09:25:43 -0400

    Pushing the data for triton serving

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ilab-triton
+
 ILAB Triton Server Configuration and Benchmarks
+
+## Serving the Server
+
+```bash
+docker run --rm --gpus all \
+  -p8000:8000 -p8001:8001 -p8002:8002 \
+  -v $(pwd)/model_repository:/models \
+  nvcr.io/nvidia/tritonserver:25.04-py3 \
+  tritonserver --model-repository=/models
+```
---
+++ ilab_triton/model_repository/identity_demo_model/identity_model_torchscript.py
@@ -0,0 +1,15 @@
+# identity_model.py
+import torch
+import torch.nn as nn
+
+class IdentityModel(nn.Module):
+    def forward(self, x):
+        return x
+
+model = IdentityModel()
+example_input = torch.randn(1, 3, 224, 224)  # Example input
+traced_model = torch.jit.trace(model, example_input)
+traced_model.save(
+    '/raid/ilab/ilab-triton/ilab_triton/model_repository/' + \
+    'identity_demo_model/1/identity_demo_model.pt'
+)
