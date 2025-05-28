8e755ed26d441a2cbff466aa7acb3d5ab283b023
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2025-05-28 11:39:53 -0400

    Adding aurora model

---
+++ ilab_triton/model_repository/weather_aurora_demo_model/weather_aurora_model_torchscript.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import torch
+import subprocess
+import numpy as np
+import urllib.request
+import torch.nn as nn
+
+# ------------------------------------------------------------------------------------
+# 1. Automatically download the model from huggingface
+# ------------------------------------------------------------------------------------
+
+# setuo url addresses
+model_url = "https://huggingface.co/microsoft/aurora/resolve/main/aurora-0.25-finetuned.ckpt"
+
+# download the model into the filesystem
+output_dir = "/raid/ilab/ilab-triton/ilab_triton/model_repository/" + \
+    "weather_aurora_demo_model/1"
+model_output_path = os.path.join(
+    output_dir, "aurora-0.25-finetuned.ckpt")
+
+# download request execution
+if not os.path.exists(model_output_path):
+    urllib.request.urlretrieve(model_url, model_output_path)
+    print(f"Downloaded to {model_output_path}")
+else:
+    print(f"Model {model_output_path} already exists.")
+
+# ------------------------------------------------------------------------------------
+# 2. Load the checkpoint of the model
+# ------------------------------------------------------------------------------------
+
+# download satvision-toa repo dependencies
+repo_url = "https://github.com/microsoft/aurora"
+repo_target_dir = "/raid/ilab/ilab-triton/ilab_triton/model_repository/" + \
+    "weather_aurora_demo_model/aurora"
+
+if not os.path.exists(repo_target_dir):
+    subprocess.run(["git", "clone", repo_url, repo_target_dir], check=True)
+    print(f"Cloned {repo_url} into {repo_target_dir}")
+else:
+    print("Repository already exists.")
+
+# setting up the path and dependencies
+sys.path.append(repo_target_dir)
+
+from aurora import Aurora, rollout
+
+model = Aurora(use_lora=False)
+model.load_checkpoint_local(model_output_path)
+model.eval().cuda()
+
+# print(f'Attempting to load checkpoint from {config.MODEL.PRETRAINED}')
+# checkpoint = torch.load(config.MODEL.PRETRAINED)
+# model.load_state_dict(checkpoint['module'])
+# print('Successfully applied checkpoint')
