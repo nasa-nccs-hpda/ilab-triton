9cb11088335d5c94b4b95de9e243250a1bad7ba7
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2025-06-12 06:35:25 -0400

    Changes to gencast loading

--- ilab_triton/model_repository/weather_gencast_demo_model/1/model.py
+++ ilab_triton/model_repository/weather_gencast_demo_model/1/model.py
@@ -7,13 +7,34 @@ import triton_python_backend_utils as pb_utils
 from saving_gencast import run_forward
 from graphcast import checkpoint, gencast
 
+import os
+import warnings
+import dataclasses
+import jax
+import numpy as np
+import xarray
+
+from graphcast import rollout
+from graphcast import xarray_jax
+from graphcast import checkpoint
+from graphcast import data_utils
+from graphcast import gencast
+
+from saving_gencast import grads_fn, run_forward
+
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 class TritonPythonModel:
 
     def initialize(self, args):
         self.params_path = "/raid/ilab/ilab-triton/ilab_triton/model_repository/weather_gencast_demo_model/1/gencast_params_GenCast_1p0deg_Mini_<2019.npz"
         with open(self.params_path, "rb") as f:
-            self.params = checkpoint.load(f, gencast.init_params())
+            # self.params = checkpoint.load(f, gencast.init_params())
+            self.ckpt = checkpoint.load(f, gencast.CheckPoint)
+            self.params = ckpt.params
+            self.state = {}
+            self.task_config = self.ckpt.task_config
+        print("LOADED GENCAST")
 
     def execute(self, requests):
         responses = []
