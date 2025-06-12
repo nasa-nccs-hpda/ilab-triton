611cf48184e3f53e3161f6549440eaab629b100d
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2025-06-12 08:40:37 -0400

    Chnages to gencast

--- ilab_triton/model_repository/weather_gencast_demo_model/1/model.py
+++ ilab_triton/model_repository/weather_gencast_demo_model/1/model.py
@@ -38,17 +38,31 @@ class TritonPythonModel:
             self.state = {}
             self.task_config = self.ckpt.task_config
         print("LOADED GENCAST")
-        self.dataset_path = '/raid/ilab/ilab-triton/ilab_triton/model_repository/weather_gencast_demo_model/gencast_dataset_source-era5_date-2019-03-29_res-1.0_levels-13_steps-01.nc'
+        self.dataset_path = os.path.join(
+            self.model_dir, "gencast_dataset_source-era5_date-2019-03-29_res-1.0_levels-13_steps-01.nc")
 
     def execute(self, requests):
         responses = []
         for request in requests:
-            nc_bytes = pb_utils.get_input_tensor_by_name(request, "input_nc").as_numpy()[0]
-            ds = xr.open_dataset(xr.backends.NetCDF4DataStore(nc_bytes))
+            #nc_bytes = pb_utils.get_input_tensor_by_name(request, "input_nc").as_numpy()[0]
+            #ds = xr.open_dataset(xr.backends.NetCDF4DataStore(nc_bytes))
+            with open(self.dataset_path, "rb") as f:
+                example_batch = xarray.load_dataset(f).compute()
 
-            result_ds = run_forward(ds, self.params)
-            result_np = result_ds.to_array().values.astype(np.float32)
+            # 2 for input, >=1 for targets
+            assert example_batch.dims["time"] >= 3
+            for var_name, var in example_batch.data_vars.items():
+                print(f"{var_name} shape: {var.shape}")
 
-            output_tensor = pb_utils.Tensor("output_array", result_np)
-            responses.append(pb_utils.InferenceResponse([output_tensor]))
+            #result_ds = run_forward(ds, self.params)
+            #result_np = result_ds.to_array().values.astype(np.float32)
+
+            #output_tensor = pb_utils.Tensor("output_array", result_np)
+            #responses.append(pb_utils.InferenceResponse([output_tensor]))
+            # Return dummy array with required shape
+            dummy_output = np.ones((8, 1, 1, 181, 360), dtype=np.float32)
+            output_tensor = pb_utils.Tensor("output_array", dummy_output)
+
+            inference_response = pb_utils.InferenceResponse(output_tensors=[output_tensor])
+            responses.append(inference_response)
         return responses
